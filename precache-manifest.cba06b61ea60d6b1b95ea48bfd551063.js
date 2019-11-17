2628b6d515ca4433086a41acf8cdb446f52cfb32
wmelodies <57869747+wmelodies@users.noreply.github.com>
2019-11-17 22:28:46 +0300

    Updates

---
+++ precache-manifest.cba06b61ea60d6b1b95ea48bfd551063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b247ef17282902997f0f89a91c19025",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccea186f2d085f7cc37e",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "d094188ef2287deba111",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ccea186f2d085f7cc37e",
+    "url": "./static/js/2.a5a6221e.chunk.js"
+  },
+  {
+    "revision": "d094188ef2287deba111",
+    "url": "./static/js/main.89fa48cd.chunk.js"
+  },
+  {
+    "revision": "9877129e602485e0b6e5",
+    "url": "./static/js/runtime-main.2e5f2a82.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
