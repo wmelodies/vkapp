2b45c23cb005e8ba7eab49155dcf07dc5150aa2c
wmelodies <57869747+wmelodies@users.noreply.github.com>
2019-12-07 16:22:20 +0300

    Updates

--- precache-manifest.96ef00e239c487dd7043e2a4eb17244d.js
+++ precache-manifest.96ef00e239c487dd7043e2a4eb17244d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d60994626e25bddc6feaaf9ebd25267",
+    "revision": "7a3b22b298dfef2a77f6a8dbc3c8dea2",
     "url": "./index.html"
   },
   {
-    "revision": "e300a289e1483eac41ee",
+    "revision": "1ac3c67aee2dff7b99bc",
     "url": "./static/css/2.9d80cd41.chunk.css"
   },
   {
-    "revision": "3ef12c2ebff7248595a8",
+    "revision": "447db8684df8fd0bbde4",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "e300a289e1483eac41ee",
-    "url": "./static/js/2.675553c1.chunk.js"
+    "revision": "1ac3c67aee2dff7b99bc",
+    "url": "./static/js/2.281acf13.chunk.js"
   },
   {
-    "revision": "3ef12c2ebff7248595a8",
-    "url": "./static/js/main.950d35c5.chunk.js"
+    "revision": "447db8684df8fd0bbde4",
+    "url": "./static/js/main.b8a8b97e.chunk.js"
   },
   {
     "revision": "9877129e602485e0b6e5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.60b2be73242149097034d7f7baf70528.js"
+  "./precache-manifest.96ef00e239c487dd7043e2a4eb17244d.js"
 );
 
 self.addEventListener('message', (event) => {
